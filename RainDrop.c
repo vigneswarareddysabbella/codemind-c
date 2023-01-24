dac799e0d819197e20d98edd2cba2bfb4d0ca3fa
vigneswarareddysabbella <119730391+vigneswarareddysabbella@users.noreply.github.com>
2023-01-24 20:00:21 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%3==0 && n%5==0)     printf("PlingPlang");     else     if(n%7==0)     printf("Plong");     else if(n%5==0)     printf("Plang");     else if(n%3==0)     printf("Pling");     else     printf("%d",n); }

---
+++ RainDrop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%3==0 && n%5==0)
+    printf("PlingPlang");
+    else
+    if(n%7==0)
+    printf("Plong");
+    else if(n%5==0)
+    printf("Plang");
+    else if(n%3==0)
+    printf("Pling");
+    else
+    printf("%d",n);
+}
