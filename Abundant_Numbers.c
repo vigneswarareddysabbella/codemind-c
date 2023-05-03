89ab48db94a9f571347435ed19df8bb2dcde84fe
vigneswarareddysabbella <119730391+vigneswarareddysabbella@users.noreply.github.com>
2023-05-03 22:44:05 +0530

    #include<stdio.h> int main() {     int a,i,d=0;     scanf("%d",&a);     for(i=1;i<a;i++)     {         if(a%i==0)         {             d=d+i;         }     }     if(d>a)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,d=0;
+    scanf("%d",&a);
+    for(i=1;i<a;i++)
+    {
+        if(a%i==0)
+        {
+            d=d+i;
+        }
+    }
+    if(d>a)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
+
