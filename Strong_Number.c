905eb166be2876eacdbcb178615b87816c98b2dc
vigneswarareddysabbella <119730391+vigneswarareddysabbella@users.noreply.github.com>
2023-05-09 07:30:29 +0530

    #include<stdio.h> int main() {     int n,m,a,i,e=1,t=0;     scanf("%d", &n);     m=n;     while(m!=0)     {         e=1;        a=m%10;        m=m/10;        for(i=a;i>0;i--)        {            e=e*i;
           }
           t=t+e;
        }
        if(t==n)
        {
            printf("The number %d is a strong number", n);
        }
        else
        {
            printf("The number %d is not a strong number", n);
        }
    }

--- Strong_Number.c
+++ Strong_Number.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n,q,r,i,wt=1,s=0;
-    scanf("%d",&n);
-    q=n;
-    while(q!=0)
+    int n,m,a,i,e=1,t=0;
+    scanf("%d", &n);
+    m=n;
+    while(m!=0)
     {
-        r=q%10;
-        for(i=1;i<=r;i++)
-        {
-            wt=wt*i;
-        }
-        s=s+wt;
-        wt=1;
-        q=q/10;
+        e=1;
+       a=m%10;
+       m=m/10;
+       for(i=a;i>0;i--)
+       {
+           e=e*i;
+           
+       }
+       t=t+e;
     }
-    if(s==n)
+    if(t==n)
     {
-        printf("The number %d is a strong number",n);
-        
+        printf("The number %d is a strong number", n);
     }
     else
     {
-        printf("The number %d is not a strong number",n);
+        printf("The number %d is not a strong number", n);
     }
 }
