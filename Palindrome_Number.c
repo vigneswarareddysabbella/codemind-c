b3a880628f0e6d80fe222e21124d990ceef2f2d0
vigneswarareddysabbella <119730391+vigneswarareddysabbella@users.noreply.github.com>
2023-05-10 19:44:00 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         int b,c,d=0,s;         scanf("%d",&b);         s=b;         while(b!=0)         {             c=b%10;             d=d*10+c;             b=b/10;         }         if(s==d)         {             printf("True ");         }         else         {             printf("False ");         }     } }

--- Palindrome_Number.c
+++ Palindrome_Number.c
@@ -1,42 +1,28 @@
 #include<stdio.h>
-int is_palndrm(int);
 int main()
 {
-	int n,nm,i;
-	scanf("%d",&nm);
-	for(i=1;i<=nm;i++)
-	{
-	scanf("%d",&n);
-	//printf("%d",is_palndrm(n));
-	if(is_palndrm(n))
-	{
-		printf("True
+    int a,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        int b,c,d=0,s;
+        scanf("%d",&b);
+        s=b;
+        while(b!=0)
+        {
+            c=b%10;
+            d=d*10+c;
+            b=b/10;
+        }
+        if(s==d)
+        {
+            printf("True
 ");
-	}
-	else
-	{
-		printf("False
+        }
+        else
+        {
+            printf("False
 ");
-	}
-	}
-	
-}
-int is_palndrm(int m)
-{
-	int q,r,s=0;
-		q=m;
-	while(q!=0)
-	{
-		r=q%10;
-		s=s*10+r;
-		q=q/10;	
-	}
-	if(m==s)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+        }
+    }
 }
