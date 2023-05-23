e13b02a0ae765fd53e8574267a5981ed430a5ddc
vigneswarareddysabbella <119730391+vigneswarareddysabbella@users.noreply.github.com>
2023-05-23 23:25:37 +0530

    #include <stdio.h>
    int power(int base, int exponent) {
        int result = 1;
        while (exponent > 0) {
            result *= base;
            exponent--;
        }
        return result;
    }
    
    int is_disarium_number(int N) {
        char str_N[20]; // Assuming the number has at most 20 digits
        sprintf(str_N, "%d", N);
        int sum_digits = 0;
        int power_value = 1;
        
        for (int i = 0; str_N[i] != NULL; i++) {
            int digit_value = str_N[i] - '0';
            int digit_power = power(digit_value, power_value);
            sum_digits += digit_power;
            power_value++;
        }
        
        return sum_digits == N;
    }
    
    int main() {
        int N;
        scanf("%d", &N);
        
        // Check if N is a Disarium number
        int result = is_disarium_number(N);
        
        // Print the result
        if (result)
            printf("True
    ");
        else
            printf("False
    ");
        
        return 0;
    }

--- Disarium_number.c
+++ Disarium_number.c
@@ -2,54 +2,43 @@
 
 int power(int base, int exponent) {
     int result = 1;
-    while (exponent != 0) {
+    while (exponent > 0) {
         result *= base;
         exponent--;
     }
     return result;
 }
 
-int isDisarium(int number) {
-    int temp = number;
-    int length = 0;
+int is_disarium_number(int N) {
+    char str_N[20]; // Assuming the number has at most 20 digits
+    sprintf(str_N, "%d", N);
+    int sum_digits = 0;
+    int power_value = 1;
     
-    // Calculate the number of digits in the number
-    while (temp != 0) {
-        length++;
-        temp /= 10;
+    for (int i = 0; str_N[i] != NULL; i++) {
+        int digit_value = str_N[i] - '0';
+        int digit_power = power(digit_value, power_value);
+        sum_digits += digit_power;
+        power_value++;
     }
     
-    int sum = 0;
-    int position = length;
-    temp = number;
-    
-    // Calculate the sum of digits raised to their respective positions
-    while (temp != 0) {
-        int digit = temp % 10;
-        sum += power(digit, position);
-        position--;
-        temp /= 10;
-    }
-    
-    // Check if the sum is equal to the number
-    if (sum == number) {
-        return 1;  // True
-    } else {
-        return 0;  // False
-    }
+    return sum_digits == N;
 }
 
 int main() {
     int N;
     scanf("%d", &N);
     
-    if (isDisarium(N)) {
+    // Check if N is a Disarium number
+    int result = is_disarium_number(N);
+    
+    // Print the result
+    if (result)
         printf("True
 ");
-    } else {
+    else
         printf("False
 ");
-    }
     
     return 0;
 }
